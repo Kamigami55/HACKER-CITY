bc91047c28790efae5bad89a37e859ce3ec2b7b5
EasonChang <samkami@icloud.com>
2016-05-21 00:18:45 +0800

    Add show method

--- app/controllers/billboard_controller.rb
+++ app/controllers/billboard_controller.rb
@@ -1,20 +1,21 @@
 class BillboardController < ApplicationController
-	def index
-		@announcements = Announcement.all
-	end
-	def  new
-		@announcement = Announcement.new
-	end
-	def create
-  		@announcement = Announcement.new(announcement_params)
-  		@announcement.save
-
-  		redirect_to :action => :index
-                end
-
-                private
-
-def announcement_params
-  params.require(:announcement).permit(:title, :date,:description)
-end
+    def index
+        @announcements = Announcement.all
+    end
+    def  new
+        @announcement = Announcement.new
+    end
+    def create
+        @announcement = Announcement.new(announcement_params)
+        @announcement.save
+        redirect_to :action => :index
+    end
+    def show
+        @announcement = Announcement.find(params[:id])
+    end
+    
+private
+    def announcement_params
+        params.require(:announcement).permit(:title, :date,:description)
+    end
 end
